0cb5c647367bebd0eaa4da9430b900dfee1a629e
Jamal Martin <jamalomartin@gmail.com>
2014-07-14 20:12:26 -0600

    adding a lib
    adding editable

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,19 +2,30 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('comicApp.controllers'));
+describe('controllers', function() {
+
+  beforeEach(module('comicApp'));
+
+  describe('adder', function() {
+    var scope;
+    beforeEach(inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      $controller('adder', {
+        $scope: scope
+      });
+    }));
+
+    it('should ....', function() {
+      expect(scope.addBook).toBeDefined();
+    });
+  })
 
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var adder = $controller('adder', { $scope: {} });
-    expect(adder).toBeDefined();
-  }));
+  
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var getComics = $controller('getComics', { $scope: {} });
-    expect(getComics).toBeDefined();
-  }));
+  // it('should ....', inject(function($controller) {
+  //   //spec body
+  //   var getComics = $controller('getComics', { $scope: {} });
+  //   expect(getComics).toBeDefined();
+  // }));
 });
