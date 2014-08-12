679c25442ce1236864f6edd186b44018d40eb2b4
Colby Natale <colby.natale@webfilings.com>
2014-08-12 15:27:54 -0600

    added namespacing; removed some console logs

--- py/delete_comic.py
+++ py/delete_comic.py
@@ -3,13 +3,16 @@ import webapp2
 import logging
 from models import Comic
 from google.appengine.ext import ndb
+from google.appengine.api import users
+from google.appengine.api import namespace_manager
 
 class DeleteComic(webapp2.RequestHandler):
 	def delete(self):
-	    key_str = self.request.url
-	    key_str = self.request.url[self.request.url.rfind('/')+1:]
-	    key = ndb.Key(urlsafe=key_str)
-	    key.delete()
+		namespace_manager.set_namespace(users.get_current_user().user_id())
+		key_str = self.request.url
+		key_str = self.request.url[self.request.url.rfind('/')+1:]
+		key = ndb.Key(urlsafe=key_str)
+		key.delete()
 
 app = webapp2.WSGIApplication([
 	('/py/delete_comic.*', DeleteComic)
--- py/record_comics.py
+++ py/record_comics.py
@@ -3,24 +3,27 @@ import webapp2
 import logging
 from models import Comic
 from google.appengine.ext import ndb
+from google.appengine.api import users
+from google.appengine.api import namespace_manager
 
 class PostComicList(webapp2.RequestHandler):
 	def post(self):
-	    comics = self.request.body
-	    logging.warning(comics)
-	    comics = json.loads(comics)
+		namespace_manager.set_namespace(users.get_current_user().user_id())
+		comics = self.request.body
+		logging.warning(comics)
+		comics = json.loads(comics)
 
-	    for comic in comics:
-	    	newComic = Comic(
-	    				publisher=comic.get('publisher'),
-	    				title=comic.get('title'),
-	    				booknum=comic.get('booknum'),
-	    				writer=comic.get('writer'),
-	    				artist=comic.get('artist'),
-	    				misc=comic.get('misc')
-	    				)
-	    	newComic.put()
-	    	self.response.out.write(newComic.key.urlsafe())
+		for comic in comics:
+			newComic = Comic(
+				publisher=comic.get('publisher'),
+				title=comic.get('title'),
+				booknum=comic.get('booknum'),
+				writer=comic.get('writer'),
+				artist=comic.get('artist'),
+				misc=comic.get('misc')
+	    	)
+			newComic.put()
+			self.response.out.write(newComic.key.urlsafe())
 
 app = webapp2.WSGIApplication([
     ('/py/record_comics', PostComicList)
